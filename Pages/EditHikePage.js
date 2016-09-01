d2b19066893c13fcee7c7d5094fd39aecfbe4905
ferris <jake@fusetools.com>
2016-09-01 14:27:57 +0200

    Added chapter 4 code

--- Pages/EditHikePage.js
+++ Pages/EditHikePage.js
@@ -1,6 +1,4 @@
-var Observable = require("FuseJS/Observable");
-
-var hike = Observable();
+var hike = this.Parameter;
 
 var name = hike.map(function(x) { return x.name; });
 var location = hike.map(function(x) { return x.location; });
@@ -8,10 +6,16 @@ var distance = hike.map(function(x) { return x.distance; });
 var rating = hike.map(function(x) { return x.rating; });
 var comments = hike.map(function(x) { return x.comments; });
 
+function goBack() {
+	router.goBack();
+}
+
 module.exports = {
 	name: name,
 	location: location,
 	distance: distance,
 	rating: rating,
-	comments: comments
+	comments: comments,
+
+	goBack: goBack
 };
--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -1,11 +1,12 @@
 var hikes = require("hikes");
 
-function chooseHike(arg) {
-	// TODO
+function goToHike(arg) {
+	var hike = arg.data;
+	router.push("editHike", hike);
 }
 
 module.exports = {
 	hikes: hikes,
 
-	chooseHike: chooseHike
+	goToHike: goToHike
 };
