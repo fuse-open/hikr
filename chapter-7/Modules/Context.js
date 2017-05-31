65a8010721a046626dd8a2c316aa3295d3c81cc0
Liam Svanåsbakken Crouch <spam@petterroea.com>
2017-05-31 18:54:52 +0200

    Applied changes to earlier chapters

--- chapter-7/Modules/Context.js
+++ chapter-7/Modules/Context.js
@@ -3,35 +3,51 @@ var Backend = require("./Backend");
 
 var hikes = Observable();
 
+function CreateObservableHike(backendHike) {
+	var obj = {};
+	obj.id = Observable(backendHike.id);
+	obj.name = Observable(backendHike.name);
+	obj.location = Observable(backendHike.location);
+	obj.distance = Observable(backendHike.distance);
+	obj.rating = Observable(backendHike.rating);
+	obj.comments = Observable(backendHike.comments);
+
+	obj.save = function() {
+		Backend.updateHike(obj.id.value, obj.name.value, obj.location.value, obj.distance.value, obj.rating.value, obj.comments.value)
+		.catch(function(error) {
+			console.log("Couldn't update hike: " + obj.id.value);
+		});
+	}
+	obj.reset = function() {
+		obj.id.value = backendHike.id;
+		obj.name.value = backendHike.name;
+		obj.location.value = backendHike.location;
+		obj.distance.value = backendHike.distance;
+		obj.rating.value = backendHike.rating;
+		obj.comments.value = backendHike.comments;
+	}
+	return obj;
+}
+function getHike(id) {
+	for(var i = 0; i < hikes.length; i++) {
+		if(hikes.getAt(i).id.value==id) {
+			return hikes.getAt(i);
+		}
+	}
+}
+
 Backend.getHikes()
 	.then(function(newHikes) {
+		for(var i = 0; i < newHikes.length; i++) {
+			newHikes[i] = CreateObservableHike(newHikes[i]);
+		}
 		hikes.replaceAll(newHikes);
 	})
 	.catch(function(error) {
 		console.log("Couldn't get hikes: " + error);
 	});
 
-function updateHike(id, name, location, distance, rating, comments) {
-	for (var i = 0; i < hikes.length; i++) {
-		var hike = hikes.getAt(i);
-		if (hike.id == id) {
-			hike.name = name;
-			hike.location = location;
-			hike.distance = distance;
-			hike.rating = rating;
-			hike.comments = comments;
-			hikes.replaceAt(i, hike);
-			break;
-		}
-	}
-	Backend.updateHike(id, name, location, distance, rating, comments)
-		.catch(function(error) {
-			console.log("Couldn't update hike: " + id);
-		});
-}
-
 module.exports = {
 	hikes: hikes,
-
-	updateHike: updateHike
+	getHike: getHike
 };
--- chapter-7/Pages/EditHikePage.js
+++ chapter-7/Pages/EditHikePage.js
@@ -1,30 +1,19 @@
 var Context = require("Modules/Context");
 
-var hike = this.Parameter;
-
-var name = hike.map(function(x) { return x.name; });
-var location = hike.map(function(x) { return x.location; });
-var distance = hike.map(function(x) { return x.distance; });
-var rating = hike.map(function(x) { return x.rating; });
-var comments = hike.map(function(x) { return x.comments; });
+var hike = this.Parameter.map(function(x) { return Context.getHike(x)});
 
 function cancel() {
-	// Refresh hike value to reset dependent Observables' values
-	hike.value = hike.value;
+	hike.value.reset();
 	router.goBack();
 }
 
 function save() {
-	Context.updateHike(hike.value.id, name.value, location.value, distance.value, rating.value, comments.value);
+	hike.value.save();
 	router.goBack();
 }
 
 module.exports = {
-	name: name,
-	location: location,
-	distance: distance,
-	rating: rating,
-	comments: comments,
+	hike: hike,
 
 	cancel: cancel,
 	save: save
