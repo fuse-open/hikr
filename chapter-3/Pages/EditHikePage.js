24353d7e647b215aeb5522962e4da8be3706bb15
Liam Svanåsbakken Crouch <spam@petterroea.com>
2017-06-01 19:18:08 +0200

    More refactoring

--- chapter-3/Pages/EditHikePage.js
+++ chapter-3/Pages/EditHikePage.js
@@ -2,16 +2,6 @@ var Observable = require("FuseJS/Observable");
 
 var hike = Observable();
 
-var name = hike.map(function(x) { return x.name; });
-var location = hike.map(function(x) { return x.location; });
-var distance = hike.map(function(x) { return x.distance; });
-var rating = hike.map(function(x) { return x.rating; });
-var comments = hike.map(function(x) { return x.comments; });
-
 module.exports = {
-	name: name,
-	location: location,
-	distance: distance,
-	rating: rating,
-	comments: comments
+	hike: hike
 };
--- chapter-4/Pages/EditHikePage.js
+++ chapter-4/Pages/EditHikePage.js
@@ -1,21 +1,11 @@
 var hike = this.Parameter;
 
-var name = hike.map(function(x) { return x.name; });
-var location = hike.map(function(x) { return x.location; });
-var distance = hike.map(function(x) { return x.distance; });
-var rating = hike.map(function(x) { return x.rating; });
-var comments = hike.map(function(x) { return x.comments; });
-
 function goBack() {
 	router.goBack();
 }
 
 module.exports = {
-	name: name,
-	location: location,
-	distance: distance,
-	rating: rating,
-	comments: comments,
+	hike: hike,
 
 	goBack: goBack
 };
