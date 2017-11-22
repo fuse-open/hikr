8a1dfeeb0c41b09c979684169294ae8c50976e33
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2017-11-22 12:23:05 +0100

    updated chapter-1 code to models

---
+++ App.js
@@ -0,0 +1,9 @@
+export default class App {
+	constructor() {
+		this.name = "Tricky Trails";
+		this.location = "Lakebed, Utah";
+		this.distance = 10.4;
+		this.rating = 4;
+		this.comments = "This hike was nice and hike-like. Glad I didn't bring a bike.";
+	}
+}
