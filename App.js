90bee3d9a1cfccac0682d38be2f1b427023ee2aa
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2017-11-22 14:52:18 +0100

    models chapter 2 code

--- App.js
+++ App.js
@@ -1,9 +1,62 @@
+class Hike {
+	constructor(name, location, distance, rating, comments) {
+		this.name = name;
+		this.location = location;
+		this.distance = distance;
+		this.rating = rating;
+		this.comments = comments;
+	}
+}
+
 export default class App {
 	constructor() {
-		this.name = "Tricky Trails";
-		this.location = "Lakebed, Utah";
-		this.distance = 10.4;
-		this.rating = 4;
-		this.comments = "This hike was nice and hike-like. Glad I didn't bring a bike.";
+		this.hike = new Hike(
+			"Tricky Trails",
+			"Lakebed, Utah",
+			10.4,
+			4,
+			"This hike was nice and hike-like. Glad I didn't bring a bike."
+		);
+		this.hikes = [
+			new Hike(
+				"Tricky Trails",
+				"Lakebed, Utah",
+				10.4,
+				4,
+				"This hike was nice and hike-like. Glad I didn't bring a bike."
+			),
+			new Hike(
+				"Mondo Mountains",
+				"Black Hills, South Dakota",
+				20.86,
+				3,
+				"Not the best, but would probably do again. Note to self: don't forget the sandwiches next time."
+			),
+			new Hike(
+				"Pesky Peaks",
+				"Bergenhagen, Norway",
+				8.2,
+				5,
+				"Short but SO sweet!!"
+			),
+			new Hike(
+				"Rad Rivers",
+				"Moriyama, Japan",
+				12.3,
+				4,
+				"Took my time with this one. Great view!"
+			),
+			new Hike(
+				"Dangerous Dirt",
+				"Cactus, Arizona",
+				19.34,
+				2,
+				"Too long, too hot. Also that snakebite wasn't very fun."
+			)
+		];
+	}
+
+	chooseHike(arg) {
+		this.hike = arg.data;
 	}
 }
